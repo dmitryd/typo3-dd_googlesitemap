dcef19236bf63f67d4a9dfb9f7df5e3e3ba5eca8
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-17 10:21:34 +0000

    Released version 1.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "dd_googlesitemap"
 #
-# Auto generated 17-03-2009 12:09
+# Auto generated 17-03-2009 12:21
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'SIA "ACCIO"',
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"87c8";s:32:"class.tx_ddgooglesitemap_eid.php";s:4:"be5c";s:34:"class.tx_ddgooglesitemap_pages.php";s:4:"958e";s:36:"class.tx_ddgooglesitemap_tcemain.php";s:4:"5321";s:35:"class.tx_ddgooglesitemap_ttnews.php";s:4:"d038";s:12:"ext_icon.gif";s:4:"0709";s:17:"ext_localconf.php";s:4:"9e3e";s:14:"ext_tables.php";s:4:"397e";s:14:"ext_tables.sql";s:4:"024e";s:24:"ext_typoscript_setup.txt";s:4:"71ba";s:14:"doc/manual.sxw";s:4:"8c8d";s:56:"renderers/class.tx_ddgooglesitemap_abstract_renderer.php";s:4:"2950";s:52:"renderers/class.tx_ddgooglesitemap_news_renderer.php";s:4:"1794";s:54:"renderers/class.tx_ddgooglesitemap_normal_renderer.php";s:4:"1040";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"a376";s:32:"class.tx_ddgooglesitemap_eid.php";s:4:"be5c";s:34:"class.tx_ddgooglesitemap_pages.php";s:4:"958e";s:36:"class.tx_ddgooglesitemap_tcemain.php";s:4:"5321";s:35:"class.tx_ddgooglesitemap_ttnews.php";s:4:"4b53";s:12:"ext_icon.gif";s:4:"0709";s:17:"ext_localconf.php";s:4:"9e3e";s:14:"ext_tables.php";s:4:"397e";s:14:"ext_tables.sql";s:4:"024e";s:24:"ext_typoscript_setup.txt";s:4:"71ba";s:14:"doc/manual.sxw";s:4:"8c8d";s:56:"renderers/class.tx_ddgooglesitemap_abstract_renderer.php";s:4:"2950";s:52:"renderers/class.tx_ddgooglesitemap_news_renderer.php";s:4:"1794";s:54:"renderers/class.tx_ddgooglesitemap_normal_renderer.php";s:4:"1040";}',
 	'suggests' => array(
 	),
 );
