28fc27bae9e7422255b971afde2f815820e42fb0
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-03-18 11:13:39 +0000

    Fixed problem: doktype and no_search were not available

--- class.tx_ddgooglesitemap_pages.php
+++ class.tx_ddgooglesitemap_pages.php
@@ -124,7 +124,7 @@ class tx_ddgooglesitemap_pages {
 			//
 			// Notice: no sorting (for speed)!
 			$this->pageList += $GLOBALS['TSFE']->sys_page->getMenu($pageInfo['uid'],
-					'uid,SYS_LASTCHANGED,tx_ddgooglesitemap_lastmod', '');
+					'uid,doktype,no_search,SYS_LASTCHANGED,tx_ddgooglesitemap_lastmod', '');
 		}
 	}
 
