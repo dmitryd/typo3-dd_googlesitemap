11d25b928aad3f7c0cd1257c9676184dd73f9469
dmitry <dmitry@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-22 11:13:33 +0000

    Released version 1.0.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "dd_googlesitemap"
+# Extension Manager/Repository config file for ext "dd_googlesitemap".
 #
-# Auto generated 18-03-2009 13:15
+# Auto generated 22-01-2010 13:12
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'High performance Google sitemap implementation that avoids typical errors by other similar extensions',
 	'category' => 'fe',
 	'author' => 'Dmitry Dulepov',
-	'author_email' => 'dmitry@typo3.org',
+	'author_email' => 'dmitry.dulepov@gmail.com',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'SIA "ACCIO"',
-	'version' => '1.0.3',
+	'version' => '1.0.4',
 	'constraints' => array(
 		'depends' => array(
 		),
@@ -38,7 +38,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"35ad";s:32:"class.tx_ddgooglesitemap_eid.php";s:4:"be5c";s:34:"class.tx_ddgooglesitemap_pages.php";s:4:"4ea2";s:36:"class.tx_ddgooglesitemap_tcemain.php";s:4:"5321";s:35:"class.tx_ddgooglesitemap_ttnews.php";s:4:"4b53";s:12:"ext_icon.gif";s:4:"0709";s:17:"ext_localconf.php";s:4:"9e3e";s:14:"ext_tables.php";s:4:"397e";s:14:"ext_tables.sql";s:4:"024e";s:24:"ext_typoscript_setup.txt";s:4:"71ba";s:14:"doc/manual.sxw";s:4:"8c8d";s:56:"renderers/class.tx_ddgooglesitemap_abstract_renderer.php";s:4:"2950";s:52:"renderers/class.tx_ddgooglesitemap_news_renderer.php";s:4:"1794";s:54:"renderers/class.tx_ddgooglesitemap_normal_renderer.php";s:4:"1040";}',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"46d3";s:32:"class.tx_ddgooglesitemap_eid.php";s:4:"5333";s:34:"class.tx_ddgooglesitemap_pages.php";s:4:"02ef";s:36:"class.tx_ddgooglesitemap_tcemain.php";s:4:"3528";s:35:"class.tx_ddgooglesitemap_ttnews.php";s:4:"0b2d";s:12:"ext_icon.gif";s:4:"0709";s:17:"ext_localconf.php";s:4:"9e3e";s:14:"ext_tables.php";s:4:"397e";s:14:"ext_tables.sql";s:4:"024e";s:24:"ext_typoscript_setup.txt";s:4:"71ba";s:14:"doc/manual.sxw";s:4:"8c8d";s:56:"renderers/class.tx_ddgooglesitemap_abstract_renderer.php";s:4:"0dc9";s:52:"renderers/class.tx_ddgooglesitemap_news_renderer.php";s:4:"9f11";s:54:"renderers/class.tx_ddgooglesitemap_normal_renderer.php";s:4:"c6ac";}',
 	'suggests' => array(
 	),
 );
