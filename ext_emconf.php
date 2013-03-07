94a9c4cf3b9af4f6771ed5135a6eb39fac3d095e
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2013-03-07 09:06:22 +0400

    Released version 1.0.8

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "dd_googlesitemap".
 #
-# Auto generated 04-12-2012 14:43
+# Auto generated 07-03-2013 09:06
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'SIA "ACCIO"',
-	'version' => '1.0.7',
+	'version' => '1.0.8',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-6.0.99',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"84c5";s:32:"class.tx_ddgooglesitemap_eid.php";s:4:"263a";s:34:"class.tx_ddgooglesitemap_pages.php";s:4:"e437";s:36:"class.tx_ddgooglesitemap_tcemain.php";s:4:"e232";s:35:"class.tx_ddgooglesitemap_ttnews.php";s:4:"c22e";s:12:"ext_icon.gif";s:4:"0709";s:17:"ext_localconf.php";s:4:"9e3e";s:14:"ext_tables.php";s:4:"397e";s:14:"ext_tables.sql";s:4:"024e";s:24:"ext_typoscript_setup.txt";s:4:"71ba";s:14:"doc/manual.sxw";s:4:"da82";s:56:"renderers/class.tx_ddgooglesitemap_abstract_renderer.php";s:4:"0dc9";s:52:"renderers/class.tx_ddgooglesitemap_news_renderer.php";s:4:"ea3b";s:54:"renderers/class.tx_ddgooglesitemap_normal_renderer.php";s:4:"c6ac";}',
+	'_md5_values_when_last_written' => 'a:19:{s:9:"ChangeLog";s:4:"822f";s:9:"README.md";s:4:"ee4d";s:32:"class.tx_ddgooglesitemap_eid.php";s:4:"d1c3";s:34:"class.tx_ddgooglesitemap_pages.php";s:4:"51cd";s:36:"class.tx_ddgooglesitemap_tcemain.php";s:4:"aedb";s:35:"class.tx_ddgooglesitemap_ttnews.php";s:4:"c00f";s:16:"ext_autoload.php";s:4:"aff4";s:12:"ext_icon.gif";s:4:"0709";s:17:"ext_localconf.php";s:4:"b549";s:14:"ext_tables.php";s:4:"eada";s:14:"ext_tables.sql";s:4:"239c";s:24:"ext_typoscript_setup.txt";s:4:"71ba";s:13:"locallang.xml";s:4:"f8ad";s:14:"doc/manual.sxw";s:4:"1e09";s:56:"renderers/class.tx_ddgooglesitemap_abstract_renderer.php";s:4:"0539";s:52:"renderers/class.tx_ddgooglesitemap_news_renderer.php";s:4:"fcb9";s:54:"renderers/class.tx_ddgooglesitemap_normal_renderer.php";s:4:"ec95";s:63:"scheduler/class.tx_ddgooglesitemap_additionalfieldsprovider.php";s:4:"4267";s:48:"scheduler/class.tx_ddgooglesitemap_indextask.php";s:4:"cbaa";}',
 	'suggests' => array(
 	),
 );
