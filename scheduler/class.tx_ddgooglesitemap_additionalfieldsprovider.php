7c768630e4f4de40d5c1c66dd76adf423f904db1
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2014-02-14 12:14:54 +0400

    Fix scheduler additional field provider for TYPO3 6.x

--- scheduler/class.tx_ddgooglesitemap_additionalfieldsprovider.php
+++ scheduler/class.tx_ddgooglesitemap_additionalfieldsprovider.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2013 Dmitry Dulepov <dmitry.dulepov@gmail.com>
+*  (c) 2013-2014 Dmitry Dulepov <dmitry.dulepov@gmail.com>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -29,19 +29,22 @@
  * - index file path (users will submit that to Google)
  * - maximum number of URLs in the sitemap (to prevent out of memory errors)
  *
+ * WARNING! Due to incompatible TYPO3 6.2 changes this class now shows PHP errors
+ * in TYPO3 4.5 and TYPO3 6.2 in the PhpStorm. These errors however do not happen
+ * at runtime.
+ *
  * @author Dmitry Dulepov <dmitry.dulepov@gmail.com>
  */
-class tx_ddgooglesitemap_additionalfieldsprovider implements tx_scheduler_AdditionalFieldProvider {
+abstract class tx_ddgooglesitemap_additionalfieldsprovider_base {
 
 	/**
 	 * Gets additional fields to render in the form to add/edit a task
 	 *
-	 * @param    array                    Values of the fields from the add/edit task form
-	 * @param    tx_ddgooglesitemap_indextask        The task object being eddited. Null when adding a task!
-	 * @param    tx_scheduler_Module        Reference to the scheduler backend module
-	 * @return    array                    A two dimensional array, array('Identifier' => array('fieldId' => array('code' => '', 'label' => '', 'cshKey' => '', 'cshLabel' => ''))
+	 * @param \tx_ddgooglesitemap_indextask $task The task object being eddited. Null when adding a task!
+	 * @param tx_scheduler_Module $schedulerModule
+	 * @return array A two dimensional array, array('Identifier' => array('fieldId' => array('code' => '', 'label' => '', 'cshKey' => '', 'cshLabel' => ''))
 	 */
-	public function getAdditionalFields(array &$taskInfo, $task, tx_scheduler_Module $schedulerModule) {
+	public function getAdditionalFields_base(tx_ddgooglesitemap_indextask $task, $schedulerModule) {
 		$additionalFields = array();
 
 		if ($schedulerModule->CMD == 'add') {
@@ -79,11 +82,10 @@ class tx_ddgooglesitemap_additionalfieldsprovider implements tx_scheduler_Additi
 	/**
 	 * Validates the additional fields' values
 	 *
-	 * @param    array                    An array containing the data submitted by the add/edit task form
-	 * @param    tx_scheduler_Module        Reference to the scheduler backend module
-	 * @return    boolean                    True if validation was ok (or selected class is not relevant), false otherwise
+	 * @param array $submittedData An array containing the data submitted by the add/edit task form
+	 * @return boolean True if validation was ok (or selected class is not relevant), false otherwise
 	 */
-	public function validateAdditionalFields(array &$submittedData, tx_scheduler_Module $schedulerModule) {
+	public function validateAdditionalFields_base(array &$submittedData) {
 		$errors = array();
 
 		$this->validateEIdUrl($submittedData, $errors);
@@ -91,8 +93,9 @@ class tx_ddgooglesitemap_additionalfieldsprovider implements tx_scheduler_Additi
 		$this->validateIndexFilePath($submittedData, $errors);
 
 		foreach ($errors as $error) {
+			/** @noinspection PhpUndefinedMethodInspection */
 			$error = $GLOBALS['LANG']->sL('LLL:EXT:dd_googlesitemap/locallang.xml:' . $error);
-			t3lib_FlashMessageQueue::addMessage(t3lib_div::makeInstance('t3lib_FlashMessage', $error, '', t3lib_FlashMessage::ERROR));
+			$this->addErrorMessage($error);
 		}
 
 		return count($errors) == 0;
@@ -105,12 +108,20 @@ class tx_ddgooglesitemap_additionalfieldsprovider implements tx_scheduler_Additi
 	 * @param tx_ddgooglesitemap_indextask $task Reference to the task
 	 * @return void
 	 */
-	public function saveAdditionalFields(array $submittedData, tx_scheduler_Task $task)
+	public function saveAdditionalFields_base(array $submittedData, tx_scheduler_Task $task)
 	{
 		$task->setEIdScriptUrl($submittedData['eIdUrl']);
 		$task->setMaxUrlsPerSitemap($submittedData['maxUrlsPerSitemap']);
 		$task->setIndexFilePath($submittedData['indexFilePath']);
 	}
+
+	/**
+	 * Adds a error message as a flash message.
+	 *
+	 * @param string $message
+	 * @return void
+	 */
+	abstract protected function addErrorMessage($message);
 
 	/**
 	 * Validates the number of urls per sitemap.
@@ -161,7 +172,9 @@ class tx_ddgooglesitemap_additionalfieldsprovider implements tx_scheduler_Additi
 			if (FALSE !== ($urlParts = parse_url($url))) {
 				if (!$urlParts['host']) {
 					$errors[] = 'scheduler.error.missingHost';
-				} else {
+				}
+				else {
+					/** @noinspection PhpUndefinedMethodInspection */
 					list($count) = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('COUNT(*) AS counter', 'sys_domain',
 							'domainName=' . $GLOBALS['TYPO3_DB']->fullQuoteStr($urlParts['host'], 'sys_domain')
 					);
@@ -176,6 +189,106 @@ class tx_ddgooglesitemap_additionalfieldsprovider implements tx_scheduler_Additi
 					$errors[] = 'scheduler.error.badParameters';
 				}
 			}
+		}
+	}
+}
+
+if (version_compare(TYPO3_branch, '6.0', '<')) {
+	class tx_ddgooglesitemap_additionalfieldsprovider extends tx_ddgooglesitemap_additionalfieldsprovider_base implements tx_scheduler_AdditionalFieldProvider {
+
+		/**
+		 * Gets additional fields to render in the form to add/edit a task
+		 *
+		 * @param array $taskInfo Values of the fields from the add/edit task form
+		 * @param tx_scheduler_task $task The task object being edited. Null when adding a task!
+		 * @param tx_scheduler_Module $schedulerModule Reference to the scheduler backend module
+		 * @return array A two dimensional array, array('Identifier' => array('fieldId' => array('code' => '', 'label' => '', 'cshKey' => '', 'cshLabel' => ''))
+		 */
+		public function getAdditionalFields(array &$taskInfo, $task, tx_scheduler_Module $schedulerModule) {
+			return parent::getAdditionalFields_base($task, $schedulerModule);
+		}
+
+		/**
+		 * Validates the additional fields' values
+		 *
+		 * @param    array                    An array containing the data submitted by the add/edit task form
+		 * @param    tx_scheduler_Module        Reference to the scheduler backend module
+		 * @return    boolean                    True if validation was ok (or selected class is not relevant), false otherwise
+		 */
+		public function validateAdditionalFields(array &$submittedData, tx_scheduler_Module $schedulerModule) {
+			return parent::validateAdditionalFields_base($submittedData);
+		}
+
+		/**
+		 * Takes care of saving the additional fields' values in the task's object
+		 *
+		 * @param array $submittedData An array containing the data submitted by the add/edit task form
+		 * @param tx_ddgooglesitemap_indextask $task Reference to the task
+		 * @return void
+		 */
+		public function saveAdditionalFields(array $submittedData, tx_scheduler_Task $task) {
+			return parent::saveAdditionalFields_base($submittedData, $task);
+		}
+
+		/**
+		 * Adds a error message as a flash message.
+		 *
+		 * @param string $message
+		 * @return void
+		 */
+		protected function addErrorMessage($message) {
+			t3lib_FlashMessageQueue::addMessage(t3lib_div::makeInstance('t3lib_FlashMessage', $message, '', t3lib_FlashMessage::ERROR));
+		}
+	}
+}
+else {
+	class tx_ddgooglesitemap_additionalfieldsprovider extends tx_ddgooglesitemap_additionalfieldsprovider_base implements \TYPO3\CMS\Scheduler\AdditionalFieldProviderInterface {
+
+		/**
+		 * Gets additional fields to render in the form to add/edit a task
+		 *
+		 * @param array $taskInfo Values of the fields from the add/edit task form
+		 * @param \TYPO3\CMS\Scheduler\Task\AbstractTask $task The task object being edited. Null when adding a task!
+		 * @param \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule Reference to the scheduler backend module
+		 * @return array A two dimensional array, array('Identifier' => array('fieldId' => array('code' => '', 'label' => '', 'cshKey' => '', 'cshLabel' => ''))
+		 */
+		public function getAdditionalFields(array &$taskInfo, $task, \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule) {
+			return parent::getAdditionalFields_base($task, $schedulerModule);
+		}
+
+		/**
+		 * Validates the additional fields' values
+		 *
+		 * @param array $submittedData An array containing the data submitted by the add/edit task form
+		 * @param \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule Reference to the scheduler backend module
+		 * @return boolean TRUE if validation was ok (or selected class is not relevant), FALSE otherwise
+		 */
+		public function validateAdditionalFields(array &$submittedData, \TYPO3\CMS\Scheduler\Controller\SchedulerModuleController $schedulerModule) {
+			return parent::validateAdditionalFields_base($submittedData);
+		}
+
+		/**
+		 * Takes care of saving the additional fields' values in the task's object
+		 *
+		 * @param array $submittedData An array containing the data submitted by the add/edit task form
+		 * @param \TYPO3\CMS\Scheduler\Task\AbstractTask $task Reference to the scheduler backend module
+		 * @return void
+		 */
+		public function saveAdditionalFields(array $submittedData, \TYPO3\CMS\Scheduler\Task\AbstractTask $task) {
+			return parent::saveAdditionalFields_base($submittedData, $task);
+		}
+
+		/**
+		 * Adds a error message as a flash message.
+		 *
+		 * @param string $message
+		 * @return void
+		 */
+		protected function addErrorMessage($message) {
+			$flashMessage = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessage',
+				$message, '', \TYPO3\CMS\Core\Messaging\FlashMessage::ERROR
+			);
+			\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Core\\Messaging\\FlashMessageQueue')->enqueue($flashMessage);
 		}
 	}
 }
