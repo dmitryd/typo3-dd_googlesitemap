d7e3153ebcdd711713165976eb4a2eea7d319036
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2014-03-07 14:10:22 +0400

    Converted scheduler classes to use namespaces

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,11 +10,11 @@ if (TYPO3_MODE == 'BE') {
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/class.tx_ddgooglesitemap_tcemain.php:&tx_ddgooglesitemap_tcemain';
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_ddgooglesitemap_indextask'] = array(
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['DmitryDulepov\\DdGooglesitemap\\Scheduler\\Task'] = array(
 	'extension'        => $_EXTKEY,
 	'title'            => 'LLL:EXT:dd_googlesitemap/locallang.xml:scheduler.title',
 	'description'      => 'LLL:EXT:dd_googlesitemap/locallang.xml:scheduler.description',
-	'additionalFields' => 'tx_ddgooglesitemap_additionalfieldsprovider'
+	'additionalFields' => 'DmitryDulepov\\DdGooglesitemap\\Scheduler\\AdditionalFieldsProvider'
 );
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['dd_googlesitemap']['sitemap']['pages'] = 'tx_ddgooglesitemap_pages->main';
