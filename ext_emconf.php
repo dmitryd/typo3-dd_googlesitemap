7ae4ef738ab0c4d7714c125dbe241a04c028284e
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2016-03-21 16:43:17 +0300

    [BUGFIX] Fix #69: composer install warnings

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 0,
   'lockType' => '',
   'author_company' => 'SIA "ACCIO"',
-  'version' => '2.0.5',
+  'version' => '2.0.6',
   'constraints' => 
   array (
     'depends' => 
