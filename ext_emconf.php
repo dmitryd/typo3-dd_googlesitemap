2473fe00628e34fc27466e7c7433ed1248a7fd01
Dmitry Dulepov <dmitry.dulepov@gmail.com>
2017-03-27 15:58:03 +0300

    [TASK] Fix #80: allow installation on TYPO3 v8

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '6.2.0-7.6.999',
+      'typo3' => '6.2.0-8.7.999',
     ),
     'conflicts' => 
     array (
